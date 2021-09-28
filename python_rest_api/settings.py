ab6f972f05abf13e4cb36a711b00fda3ac6478dc
Hemant Chhonkar <hemant.chhonkar@gmail.com>
2021-09-28 16:20:39 +0530

    Update settings.py

--- python_rest_api/settings.py
+++ python_rest_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l3&iow@+^eod1jzo23@=7w@55vats(c9=!fwf6@%zpi51z7fbr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.14.145.64','localhost']
 
 
 # Application definition
